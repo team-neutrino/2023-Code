7ffd5e0c0e73cf8ff613df4e95a07c498f82e491
gurellia53 <70857168+Noah9847@users.noreply.github.com>
2023-01-19 19:00:40 -0600

    y6

--- 2023-Code/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -17,49 +17,37 @@ public class DriveTrainSubsystem extends SubsystemBase {
       new CANSparkMax(Constants.MotorConstants.RMOTOR1, MotorType.kBrushless);
   private CANSparkMax m_rmotor2 =
       new CANSparkMax(Constants.MotorConstants.RMOTOR2, MotorType.kBrushless);
-  private CANSparkMax m_rmotor3 =
-      new CANSparkMax(Constants.MotorConstants.RMOTOR3, MotorType.kBrushless);
   private CANSparkMax m_lmotor1 =
       new CANSparkMax(Constants.MotorConstants.LMOTOR1, MotorType.kBrushless);
   private CANSparkMax m_lmotor2 =
       new CANSparkMax(Constants.MotorConstants.LMOTOR2, MotorType.kBrushless);
-  private CANSparkMax m_lmotor3 =
-      new CANSparkMax(Constants.MotorConstants.LMOTOR3, MotorType.kBrushless);
 
   private RelativeEncoder m_encoderR1;
   private RelativeEncoder m_encoderR2;
-  private RelativeEncoder m_encoderR3;
   private RelativeEncoder m_encoderL1;
   private RelativeEncoder m_encoderL2;
-  private RelativeEncoder m_encoderL3;
 
   double DEADZONE = 0.1;
-  MotorControllerGroup m_rmotors = new MotorControllerGroup(m_rmotor1, m_rmotor2, m_rmotor3);
-  MotorControllerGroup m_lmotors = new MotorControllerGroup(m_lmotor1, m_lmotor2, m_lmotor3);
+  MotorControllerGroup m_rmotors = new MotorControllerGroup(m_rmotor1, m_rmotor2);
+  MotorControllerGroup m_lmotors = new MotorControllerGroup(m_lmotor1, m_lmotor2);
 
   /** Creates a new Drivetrain. */
   public DriveTrainSubsystem() {
 
     m_rmotor1.restoreFactoryDefaults();
     m_rmotor2.restoreFactoryDefaults();
-    m_rmotor3.restoreFactoryDefaults();
     m_lmotor1.restoreFactoryDefaults();
     m_rmotor2.restoreFactoryDefaults();
-    m_lmotor3.restoreFactoryDefaults();
 
     m_rmotor1.setInverted(true);
     m_rmotor2.setInverted(true);
-    m_rmotor3.setInverted(true);
     m_lmotor1.setInverted(false);
     m_lmotor2.setInverted(false);
-    m_lmotor3.setInverted(true);
 
     m_encoderR1 = m_rmotor1.getEncoder();
     m_encoderR2 = m_rmotor2.getEncoder();
-    m_encoderR3 = m_rmotor3.getEncoder();
     m_encoderL1 = m_lmotor1.getEncoder();
     m_encoderL2 = m_lmotor2.getEncoder();
-    m_encoderL3 = m_lmotor3.getEncoder();
   }
 
   public double getR1Pos() {
@@ -70,20 +58,12 @@ public class DriveTrainSubsystem extends SubsystemBase {
     return m_encoderR2.getPosition();
   }
 
-  public double getR3Pos() {
-    return m_encoderR3.getPosition();
-  }
-
   public double getL1Pos() {
     return m_encoderL1.getPosition();
   }
 
   public double getL2Pos() {
     return m_encoderL2.getPosition();
-  }
-
-  public double getL3Pos() {
-    return m_encoderL3.getPosition();
   }
 
   public double getR1Vel() {
@@ -94,10 +74,6 @@ public class DriveTrainSubsystem extends SubsystemBase {
     return m_encoderR2.getVelocity();
   }
 
-  public double getR3Vel() {
-    return m_encoderR3.getVelocity();
-  }
-
   public double getL1Vel() {
     return m_encoderL1.getVelocity();
   }
@@ -106,15 +82,13 @@ public class DriveTrainSubsystem extends SubsystemBase {
     return m_encoderL2.getVelocity();
   }
 
-  public double getL3Vel() {
-    return m_encoderL3.getVelocity();
-  }
-
   public void setMotors(double m_rightMotorSpeed, double m_leftMotorSpeed) {
-    m_leftMotorSpeed = linearAccel(deadzone(m_leftMotorSpeed));
-    m_rightMotorSpeed = linearAccel(deadzone(m_rightMotorSpeed));
-    m_rmotors.set(m_rightMotorSpeed);
-    m_lmotors.set(m_leftMotorSpeed);
+    double leftMotorSpeed = linearAccel(deadzone(m_leftMotorSpeed));
+    double rightMotorSpeed = linearAccel(deadzone(m_rightMotorSpeed));
+    // m_rmotors.set(rightMotorSpeed);
+    // m_lmotors.set(leftMotorSpeed);
+    m_rmotor1.set(1);
+    m_rmotor2.set(1);
   }
 
   public double deadzone(double joystickY) {
