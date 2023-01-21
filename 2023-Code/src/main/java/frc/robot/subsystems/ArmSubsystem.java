5459ef4f73d580c4921cc8b4b5a278c3bd7742da
Neutrino Controls <daviddongprog@gmail.com>
2023-01-21 12:17:02 -0600

    update from main

--- 2023-Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -9,31 +9,22 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ArmSubsystem extends SubsystemBase {
 
-  private CANSparkMax m_armMotor1 = new CANSparkMax(Constants.MotorConstants.ARM_MOTOR1, MotorType.kBrushless);
-  private CANSparkMax m_armMotor2 = new CANSparkMax(Constants.MotorConstants.ARM_MOTOR2, MotorType.kBrushless);
-
-  private RelativeEncoder m_encoder = m_armMotor1.getEncoder();
-  //private MotorControllerGroup m_motorGroup;
-
+  private CANSparkMax m_armMotor = new CANSparkMax(Constants.MotorConstants.ARM_MOTOR1, MotorType.kBrushless);
+  private RelativeEncoder m_encoder = m_armMotor.getEncoder();
   private SparkMaxPIDController m_pidController;
 
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
-    m_armMotor1.restoreFactoryDefaults();
-    m_armMotor2.restoreFactoryDefaults();
-    m_armMotor2.follow(m_armMotor1);
-    //m_motorGroup = new MotorControllerGroup(m_armMotor1, m_armMotor2);
-    m_pidController = m_armMotor2.getPIDController();
+    m_armMotor.restoreFactoryDefaults();
+    m_pidController = m_armMotor.getPIDController();
     m_encoder.setPositionConversionFactor(Constants.ArmConstants.ROTATION_TO_INCHES);
-    //m_pidController = new SparkMaxPIDController(m_armMotor1);
     m_pidController.setFeedbackDevice(m_encoder);
+
     m_pidController.setP(Constants.PIDConstants.ARM_P);
     m_pidController.setI(Constants.PIDConstants.ARM_I);
     m_pidController.setD(Constants.PIDConstants.ARM_D);
@@ -44,20 +35,8 @@ public class ArmSubsystem extends SubsystemBase {
     );
   }
 
-  public void setSpeedArmMotor1(double speed) {
-    m_armMotor1.set(speed);
-  }
-
-  public void setSpeedArmMotor2(double speed) {
-    m_armMotor2.set(speed);
-  }
-
-  public double getSpeedArmMotor1() {
-    return m_armMotor1.get();
-  }
-
-  public double getSpeedArmMotor2() {
-    return m_armMotor2.get();
+  public void setSpeed(double speed) {
+    m_armMotor.set(speed);
   }
 
   public void setReference(double rotations) {
@@ -65,8 +44,14 @@ public class ArmSubsystem extends SubsystemBase {
     m_pidController.setReference(rotations, ControlType.kPosition);
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public double getSpeed() {
+    return m_armMotor.get();
   }
+
+  public double getPosition() {
+    return m_encoder.getPosition();
+  }
+
+  @Override
+  public void periodic() {}
 }
