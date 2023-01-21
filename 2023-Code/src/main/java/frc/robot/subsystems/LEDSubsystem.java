96a4c79b5de6abf04403cf0ff75eb104f163a9f7
gurellia53 <70857168+Noah9847@users.noreply.github.com>
2023-01-20 18:45:27 -0600

    Update LEDSubsystem.java

--- 2023-Code/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -4,14 +4,37 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase {
+
+  public AddressableLED m_addressableLED = new AddressableLED(9);
+  public AddressableLEDBuffer m_LedBuffer = new AddressableLEDBuffer(60);
+
+  public void setToPurple(){
+    for (var i = 0; i < m_LedBuffer.getLength(); i++){
+      m_LedBuffer.setRGB(i, 95, 0, 160);
+    }
+  }
+
+  public void start(){}
+
+  public void stop(){}
+
   /** Creates a new LEDSubsystem. */
-  public LEDSubsystem() {}
+  public LEDSubsystem() {
+    m_addressableLED.setLength(500);
+    m_addressableLED.setData(m_LedBuffer);
+    m_addressableLED.start();
+
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    setToPurple();
+
   }
 }
