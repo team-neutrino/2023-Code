6eec0703ba1cd47a3cf383d5f6fa33b3b6846f6a
Neutrino Controls <daviddongprog@gmail.com>
2023-01-23 17:42:35 -0600

    Update ShuffleboardSubsystem.java

--- 2023-Code/src/main/java/frc/robot/subsystems/ShuffleboardSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/ShuffleboardSubsystem.java
@@ -20,19 +20,21 @@ public class ShuffleboardSubsystem extends SubsystemBase {
   ScoringSubsystem m_scoring;
   DriveTrainSubsystem m_driveTrain;
   EndGameSubsystem m_endGame;
+  LimelightSubsystem m_limelight;
 
   /** Creates a new ShuffleboardSubsystem. */
   public ShuffleboardSubsystem(
-      ScoringSubsystem p_scoring, DriveTrainSubsystem p_driveTrain, EndGameSubsystem p_endGame) {
+      ScoringSubsystem p_scoring, DriveTrainSubsystem p_driveTrain, EndGameSubsystem p_endGame, LimelightSubsystem p_limelight) {
     m_scoring = p_scoring;
     m_driveTrain = p_driveTrain;
     m_endGame = p_endGame;
+    m_limelight = p_limelight;
     m_drivestationTab = Shuffleboard.getTab("driverstation tab");
 
     driveStaionTab();
   }
 
-  @Override
+  @Override 
   public void periodic() {
     m_scoringVariables[0].setBoolean(m_scoring.getSolenoidValue());
     m_driveTrainVariables[0].setDouble(m_driveTrain.getR1Pos());
@@ -43,12 +45,21 @@ public class ShuffleboardSubsystem extends SubsystemBase {
     m_driveTrainVariables[5].setDouble(m_driveTrain.getR2Vel());
     m_driveTrainVariables[6].setDouble(m_driveTrain.getL1Vel());
     m_driveTrainVariables[7].setDouble(m_driveTrain.getL2Vel());
+    m_endGameVariables[0].setBoolean(m_endGame.getSolenoidvalueFront());
     m_endGameVariables[1].setBoolean(m_endGame.getSolenoidvalueBack());
+    m_limelightVariables[0].setDouble(m_limelight.getID());
+    m_limelightVariables[1].setDouble(m_limelight.getTx());
+    m_limelightVariables[2].setDouble(m_limelight.getTy());
+    m_limelightVariables[3].setDouble(m_limelight.getCamTran());
+    m_limelightVariables[4].getDouble(m_limelight.getDistance());
   }
 
   private void driveStaionTab() {
     m_drivestationTab = Shuffleboard.getTab("Drivestation Tab");
     m_endGameVariables[1] =
         m_drivestationTab.add("isBlue", false).withPosition(6, 0).withSize(1, 1).getEntry();
+    m_limelightVariables[4] =
+        m_drivestationTab.add("Distance", 0).withPosition(4, 0).withSize(1, 1).getEntry();
+    
   }
 }
