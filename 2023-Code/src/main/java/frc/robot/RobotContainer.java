38699f0f23735b0244b071a8eee2908441b33e88
Neutrino Controls <77417861+ddavidd23@users.noreply.github.com>
2023-01-25 18:20:19 -0600

    appease

--- 2023-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Code/src/main/java/frc/robot/RobotContainer.java
@@ -27,7 +27,6 @@ import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.EndGameSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.LimelightSubsystem;
 import frc.robot.subsystems.PneumaticsSubsystem;
 import frc.robot.subsystems.ScoringSubsystem;
@@ -47,7 +46,6 @@ public class RobotContainer {
   private final ArmSubsystem m_armSubsystem = new ArmSubsystem();
   private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
   private final ScoringSubsystem m_scoringSubsystem = new ScoringSubsystem();
-  private final LEDSubsystem m_LedSubsystem;
   private final ShuffleboardSubsystem m_shuffleboardSubsystem =
       new ShuffleboardSubsystem(m_scoringSubsystem, m_driveTrain, m_endGame);
 
@@ -119,8 +117,6 @@ public class RobotContainer {
     new LimelightSubsystem();
 
     configureBindings();
-
-    m_LedSubsystem = new LEDSubsystem();
   }
 
   private void configureBindings() {
