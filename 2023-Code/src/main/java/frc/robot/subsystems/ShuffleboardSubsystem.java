dbbda076a0df2938598bd36df19e3e6956b0fde2
Neutrino Controls <77417861+ddavidd23@users.noreply.github.com>
2023-01-25 19:22:47 -0600

    Shuffleboard testing stuff

--- 2023-Code/src/main/java/frc/robot/subsystems/ShuffleboardSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/ShuffleboardSubsystem.java
@@ -12,27 +12,75 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ShuffleboardSubsystem extends SubsystemBase {
   private ShuffleboardTab m_drivestationTab;
   private GenericEntry m_driveTrainVariables[] = new GenericEntry[7];
+  private GenericEntry m_limelightVariables[] = new GenericEntry[8];
+  private GenericEntry m_driverStationInfo[] = new GenericEntry[9];
+  private GenericEntry m_scoringVariables[] = new GenericEntry[11];
 
   private DriveTrainSubsystem m_driveTrain;
+  private LimelightSubsystem m_limelight;
+  private ScoringSubsystem m_scoring;
+  private EndGameSubsystem m_endGame;
 
-  public ShuffleboardSubsystem(DriveTrainSubsystem p_driveTrain) {
+  public ShuffleboardSubsystem(DriveTrainSubsystem p_driveTrain, ScoringSubsystem p_scoring, LimelightSubsystem p_limelight) {
+
     m_driveTrain = p_driveTrain;
+    m_limelight = p_limelight; 
+    m_scoring = p_scoring;
     m_drivestationTab = Shuffleboard.getTab("Driverstation Tab");
 
-    driveStaionTab();
+    driveStationTab();
   }
 
   @Override
   public void periodic() {
     m_driveTrainVariables[0].setDouble(m_driveTrain.getR1Vel());
+    m_scoringVariables[0].setBoolean(m_scoring.getSolenoidValue());
+    m_driveTrainVariables[8].setDouble(m_driveTrain.getR1Pos());
+    m_driveTrainVariables[1].setDouble(m_driveTrain.getR2Pos());
+    m_driveTrainVariables[2].setDouble(m_driveTrain.getL1Pos());
+    m_driveTrainVariables[3].setDouble(m_driveTrain.getL2Pos());
+    // m_driveTrainVariables[4].setDouble(m_driveTrain.getR1Vel());
+    m_driveTrainVariables[5].setDouble(m_driveTrain.getR2Vel());
+    m_driveTrainVariables[6].setDouble(m_driveTrain.getL1Vel());
+    m_driveTrainVariables[7].setDouble(m_driveTrain.getL2Vel());
+
   }
 
-  private void driveStaionTab() {
+  private void driveStationTab() {
+    m_drivestationTab = Shuffleboard.getTab("Drivestation Tab");
+
     m_driveTrainVariables[0] =
         m_drivestationTab
-            .add("Right Motor 1 Position", 0)
+            .add("Right Motor 1 Velocity", 0)
             .withPosition(0, 0)
             .withSize(1, 1)
             .getEntry();
-  }
+
+    m_driveTrainVariables[1] = 
+      m_drivestationTab.add("Right Motor 2 Position", 0).withPosition(0,0).withSize(1,1).getEntry();
+
+    m_driveTrainVariables[2] = 
+      m_drivestationTab.add("Left Motor 1 Position", 0).withPosition(0,0).withSize(1,1).getEntry();
+    
+    m_driveTrainVariables[3] = 
+      m_drivestationTab.add("Left Motor 2 Position", 0).withPosition(0,0).withSize(1,1).getEntry();
+
+    m_driveTrainVariables[5] = 
+      m_drivestationTab.add("Right Motor 2 Velocity", 0).withPosition(0,0).withSize(1,1).getEntry();
+
+    m_driveTrainVariables[6] = 
+      m_drivestationTab.add("Left Motor 1 Velocity", 0).withPosition(0,0).withSize(1,1).getEntry();
+      
+    m_driveTrainVariables[7] = 
+      m_drivestationTab.add("Left Motor 2 Velocity", 0).withPosition(0,0).withSize(1,1).getEntry();
+
+
+    m_limelightVariables[0] = 
+      m_drivestationTab.add("Distance", 0).withPosition(7, 0).withSize(1, 1).getEntry();
+
+    m_driverStationInfo[0] = 
+      m_drivestationTab.add("Match Time", 0).withPosition(0,0).withSize(3,1).getEntry();
+
+
+}
 }
