b7658eea0ee48067636196eb2e6cce1cd6c4ee3d
Neutrino Controls <123705736+ViennaRossmanith@users.noreply.github.com>
2023-01-27 19:51:53 -0600

    567890

--- 2023-Code/src/main/java/frc/robot/subsystems/ShuffleboardSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/ShuffleboardSubsystem.java
@@ -22,6 +22,9 @@ public class ShuffleboardSubsystem extends SubsystemBase {
   private GenericEntry m_driverStationInfoVariables[] = new GenericEntry[9];
   private GenericEntry m_scoringVariables[] = new GenericEntry[11];
   private GenericEntry m_armVariables[] = new GenericEntry[1];
+  private GenericEntry m_intakeVariables[] = new GenericEntry[4];
+
+
 
   private DriveTrainSubsystem m_driveTrain;
   private LimelightSubsystem m_limelight;
@@ -30,17 +33,20 @@ public class ShuffleboardSubsystem extends SubsystemBase {
   private DriverStationInfo m_driverStationInfo;
   private ArmSubsystem m_arm;
   private HttpCamera LLFeed;
+  private IntakeSubsystem m_intake;
   public ShuffleboardSubsystem(
       DriverStationInfo p_driverStationInfo,
       DriveTrainSubsystem p_driveTrain,
       ScoringSubsystem p_scoring,
       LimelightSubsystem p_limelight,
-      ArmSubsystem p_arm) {
+      ArmSubsystem p_arm,
+      IntakeSubsystem p_intake) {
 
     m_driveTrain = p_driveTrain;
     m_limelight = p_limelight;
     m_scoring = p_scoring;
     m_arm = p_arm;
+    m_intake = p_intake;
     m_driverStationInfo = p_driverStationInfo;
     m_drivestationTab = Shuffleboard.getTab("Driverstation Tab");
 
@@ -49,14 +55,20 @@ public class ShuffleboardSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    m_driveTrainVariables[0].setDouble(m_driveTrain.getR1Pos());
-    m_driveTrainVariables[1].setDouble(m_driveTrain.getR2Pos());
-    m_driveTrainVariables[2].setDouble(m_driveTrain.getL1Pos());
-    m_driveTrainVariables[3].setDouble(m_driveTrain.getL2Pos());
-    m_driveTrainVariables[4].setDouble(m_driveTrain.getR1Vel());
+   // m_driveTrainVariables[0].setDouble(m_driveTrain.getR1Pos());
+    //m_driveTrainVariables[1].setDouble(m_driveTrain.getR2Pos());
+    //m_driveTrainVariables[2].setDouble(m_driveTrain.getL1Pos());
+    //m_driveTrainVariables[3].setDouble(m_driveTrain.getL2Pos());
+    m_driveTrainVariables[4].setDouble(m_driveTrain.getR1Vel());  
     m_driveTrainVariables[5].setDouble(m_driveTrain.getR2Vel());
     m_driveTrainVariables[6].setDouble(m_driveTrain.getL1Vel());
     m_driveTrainVariables[7].setDouble(m_driveTrain.getL2Vel());
+
+    m_intakeVariables[4].setBoolean(m_intake.getUpDownSolenoidValue());
+    m_intakeVariables[3].setBoolean(m_intake.getSqueezeSolenoidValue());
+    m_scoringVariables[2].setBoolean(m_scoring.getSolenoidValue());
+
+
     
     m_driverStationInfoVariables[0].setDouble(m_driverStationInfo.getMatchTime());
 
@@ -66,40 +78,40 @@ public class ShuffleboardSubsystem extends SubsystemBase {
   private void driveStationTab() {
     m_drivestationTab = Shuffleboard.getTab("Drivestation Tab123");
 
-    m_driveTrainVariables[0] =
+    /*m_driveTrainVariables[0] =
         m_drivestationTab
             .add("Right Motor 1 Position", 0)
             .withPosition(0, 0)
             .withSize(1, 1)
-            .getEntry();
+            .getEntry();/* */
 
-    m_driveTrainVariables[1] =
+    /*m_driveTrainVariables[1] =
         m_drivestationTab
             .add("Right Motor 2 Position", 0)
             .withPosition(0, 1)
             .withSize(1, 1)
-            .getEntry();
+            .getEntry();/* */
 
-    m_driveTrainVariables[2] =
+    /*m_driveTrainVariables[2] =
         m_drivestationTab
             .add("Left Motor 1 Position", 0)
             .withPosition(0, 2)
             .withSize(1, 1)
-            .getEntry();
+            .getEntry();/* */
 
-    m_driveTrainVariables[3] =
+    /*m_driveTrainVariables[3] =
         m_drivestationTab
             .add("Left Motor 2 Position", 0)
             .withPosition(0, 3)
             .withSize(1, 1)
-            .getEntry();
+            .getEntry();/* */
 
     m_driveTrainVariables[4] =
-            m_drivestationTab
-                .add("Right Motor 1 Velocity", 0)
-                .withPosition(0, 3)
-                .withSize(1, 1)
-                .getEntry();
+        m_drivestationTab
+            .add("Right Motor 1 Velocity", 0)
+            .withPosition(0, 3)
+            .withSize(1, 1)
+            .getEntry();
 
     m_driveTrainVariables[5] =
         m_drivestationTab
@@ -121,23 +133,53 @@ public class ShuffleboardSubsystem extends SubsystemBase {
             .withPosition(0, 6)
             .withSize(1, 1)
             .getEntry();
+    
+    m_scoringVariables[2] =
+        m_drivestationTab
+            .add("Grabber Release", 0)
+            .withPosition(0, 6)
+            .withSize(1, 1)
+            .getEntry();
+
+    m_scoringVariables[4] =
+        m_drivestationTab
+            .add("Intake Down ", 0)
+            .withPosition(0, 6)
+            .withSize(1, 1)
+            .getEntry();
+
+    m_scoringVariables[3] =
+        m_drivestationTab
+            .add("Squeeze", 0)
+            .withPosition(0, 6)
+            .withSize(1, 1)
+            .getEntry();
 
     m_limelightVariables[0] =
-        m_drivestationTab.add("Distance", 0).withPosition(1, 0).withSize(1, 1).getEntry();
+        m_drivestationTab
+            .add("Distance", 0)
+            .withPosition(1, 0)
+            .withSize(1, 1)
+            .getEntry();
 
     m_driverStationInfoVariables[0] =
-        m_drivestationTab.add("Match Time", 0).withPosition(2, 0).withSize(3, 1).getEntry();
+        m_drivestationTab
+            .add("Match Time", 0)
+            .withPosition(2, 0)
+            .withSize(3, 1)
+            .getEntry();
 
     m_armVariables[0] =
-        m_drivestationTab.add("Arm Position", 0).withPosition(3, 0).withSize(1, 1).getEntry();
+        m_drivestationTab
+            .add("Arm Position", 0)
+            .withPosition(3, 0)
+            .withSize(1, 1)
+            .getEntry();
     
         try {
           LLFeed =
               new HttpCamera(
                   "limelight", "http://10.39.28.92", HttpCameraKind.kMJPGStreamer);
-                  //limelight.local>:5800
-                  http://10.39.28.92
-                  //http://limelight.local:5800/stream.mjpg
           CameraServer.startAutomaticCapture(LLFeed);
           m_drivestationTab
               .add(LLFeed)
