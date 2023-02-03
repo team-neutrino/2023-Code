3338151f85a35ea976a6a2ae54eec6b94792dd0b
gurellia53 <123705736+ViennaRossmanith@users.noreply.github.com>
2023-02-03 17:00:19 -0600

    test for toggle

--- 2023-Code/src/main/java/frc/robot/RobotContainer.java
+++ 2023-Code/src/main/java/frc/robot/RobotContainer.java
@@ -92,7 +92,7 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     // m_buttonA.whileTrue(m_scoringOpenCommand);
-    m_buttonA.whileTrue(m_DriveTrainDriveFowardCommand);
+    m_buttonA.toggleOnTrue(m_DriveTrainDriveFowardCommand);
     m_buttonB.whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
