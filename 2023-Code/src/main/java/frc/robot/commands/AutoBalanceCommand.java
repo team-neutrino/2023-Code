3540dc86f129f0ca757a7dcf47b714e38e48e3c7
David Dong <123705736+ViennaRossmanith@users.noreply.github.com>
2023-02-04 14:39:24 -0600

    Update AutoBalanceCommand.java

--- 2023-Code/src/main/java/frc/robot/commands/AutoBalanceCommand.java
+++ 2023-Code/src/main/java/frc/robot/commands/AutoBalanceCommand.java
@@ -8,17 +8,17 @@ public class AutoBalanceCommand extends CommandBase {
 
   private final DriveTrainSubsystem m_drivetrain;
 
-  double ish = 0.4;
-  double desiredPos = 0;
-  double error;
-  double voltage;
-
-  double integral = 0;
-
-  double derivative = 0;
-  double previousError = 0;
+  final double ish = 0.4;
   final double dt = .02;
 
+  double desiredPos = 0;
+  double error;
+  double previousError = 0;
+  
+  double voltage;
+  double integral = 0;
+  double derivative = 0;
+  
   public AutoBalanceCommand(DriveTrainSubsystem p_drivetrain) {
     m_drivetrain = p_drivetrain;
     addRequirements(m_drivetrain);
