ac6af709e34ad2a2eeb205273a56cdbe46706705
Leslie714 <117839284+ssarahyao@users.noreply.github.com>
2023-02-04 14:51:46 -0600

    slowly

--- 2023-Code/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -20,7 +20,7 @@ public class LEDSubsystem extends SubsystemBase {
   /** Creates a new LEDSubsystem. */
   public LEDSubsystem() {
     m_addressableLED = new AddressableLED(0);
-    m_LEDBuffer = new AddressableLEDBuffer(8);
+    m_LEDBuffer = new AddressableLEDBuffer(10);
 
     m_addressableLED.setLength(m_LEDBuffer.getLength());
     m_addressableLED.setData(m_LEDBuffer);
@@ -65,9 +65,9 @@ public class LEDSubsystem extends SubsystemBase {
 
   private void sarahStrobe() {
     double timeConst = Math.PI;
-    int r = (int) Math.round(126 * Math.cos(timeConst * timer.get()) + 126);
-    int g = (int) Math.round(126 * Math.cos(timeConst / 2 * timer.get()) + 126);
-    int b = (int) Math.round(64 * Math.sin(2 * timeConst * timer.get()) + 64);
+    int r = (int) Math.round(126 * Math.cos(timeConst / 4* timer.get()) + 126);
+    int g = (int) Math.round(126 * Math.cos(timeConst / 8 * timer.get()) + 126);
+    int b = (int) Math.round(126 * Math.sin(timeConst / 2 * timer.get()) + 126);
     for (int i = 0; i < m_LEDBuffer.getLength(); ++i) setToColor(r, g, b);
   }
 
