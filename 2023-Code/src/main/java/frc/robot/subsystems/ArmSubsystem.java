803d9a1860522579eedf988dbd5ade4d2718560c
Neutrino Controls <714.leslie.kim@gmail.com>
2023-02-04 17:23:36 -0600

    remove a print

--- 2023-Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -79,7 +79,6 @@ public class ArmSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    System.out.println(getVoltage());
     // System.out.println("ENCODER: " + getAbsolutePosition());
   }
 }
