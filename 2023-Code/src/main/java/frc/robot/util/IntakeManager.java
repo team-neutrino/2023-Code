722ee17d2d45dc34dd09334fac3d69fae613ff87
Neutrino Controls <daviddongprog@gmail.com>
2023-02-07 19:28:01 -0600

    to teach

--- 2023-Code/src/main/java/frc/robot/util/IntakeManager.java
+++ 2023-Code/src/main/java/frc/robot/util/IntakeManager.java
@@ -7,10 +7,15 @@ public class IntakeManager {
     IntakeSubsystem m_intakeSubsystem;
     ArmSubsystem m_armSubsystem; 
 
-    public IntakeManager(IntakeSubsystem p_intakeSubsystem, ArmSubsystem p_armSubsystem){
-        m_intakeSubsystem = p_intakeSubsystem;
+    public IntakeManager(IntakeSubsystem x, ArmSubsystem p_armSubsystem){
+        m_intakeSubsystem = x;
         m_armSubsystem = p_armSubsystem;
     }
 
+    public void doSomething()
+    {
+        //
+        m_intakeSubsystem.setIntakeDown();
+    }
     
 }
