5254bf3fa8ad510cdbc20111ad648bc186df5214
Neutrino Controls <daviddongprog@gmail.com>
2023-02-08 19:57:04 -0600

    suppresion

--- 2023-Code/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -6,75 +6,67 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.util.LEDColor;
 
 public class LEDSubsystem extends SubsystemBase {
 
   public AddressableLED m_addressableLED;
-  public AddressableLEDBuffer m_LEDBuffer;
-  private Timer timer = new Timer();
+  public AddressableLEDBuffer m_LedBuffer;
+  private boolean m_beamBreak;
+
+  public void start() {}
+
+  public void stop() {}
 
   /** Creates a new LEDSubsystem. */
   public LEDSubsystem() {
     m_addressableLED = new AddressableLED(0);
-    m_LEDBuffer = new AddressableLEDBuffer(10);
+    m_LedBuffer = new AddressableLEDBuffer(8);
 
-    m_addressableLED.setLength(m_LEDBuffer.getLength());
-    m_addressableLED.setData(m_LEDBuffer);
+    m_addressableLED.setLength(m_LedBuffer.getLength());
+    m_addressableLED.setData(m_LedBuffer);
     m_addressableLED.start();
-    setToOrange();
-    timer.start();
-  }
-
-  private void setToColor(int r, int g, int b) {
-    for (int i = 0; i < m_LEDBuffer.getLength(); i++) {
-      m_LEDBuffer.setRGB(i, r, g, b);
-    }
+    m_LedBuffer.setRGB(0, 256, 0, 0);
   }
 
   public void setToPurple() {
-    setToColor(210, 25, 210);
-  }
-
-  public void setToYellow() {
-    setToColor(255, 100, 0);
-  }
-
-  public void setToOrange() {
-    setToColor(255, 15, 0);
-  }
-
-  public LEDColor getColor() {
-    Color orange = new Color(255, 15, 0);
-    Color purple = new Color(210, 25, 210);
-    Color yellow = new Color(255, 100, 0);
-
-    if (m_LEDBuffer.getLED(1).equals(orange)) {
-      return LEDColor.ORANGE;
-    } else if (m_LEDBuffer.getLED(1).equals(purple)) {
-      return LEDColor.PURPLE;
-    } else if (m_LEDBuffer.getLED(1).equals(yellow)) {
-      return LEDColor.YELLOW;
-    } else {
-      return LEDColor.INDETERMINATE;
+    for (int i = 0; i < m_LedBuffer.getLength(); i++) {
+      m_LedBuffer.setRGB(i, 162, 25, 255);
     }
   }
 
-  private void sarahStrobe() {
-    double timeConst = Math.PI;
-    int r = (int) Math.round(126 * Math.cos(timeConst / 4 * timer.get()) + 126);
-    int g = (int) Math.round(126 * Math.cos(timeConst / 8 * timer.get()) + 126);
-    int b = (int) Math.round(126 * Math.sin(timeConst / 2 * timer.get()) + 126);
-    for (int i = 0; i < m_LEDBuffer.getLength(); ++i) setToColor(r, g, b);
+  public void setToYellow() {
+    for (int i = 0; i < m_LedBuffer.getLength(); i++) {
+      m_LedBuffer.setRGB(i, 255, 100, 0);
+    }
+  }
+
+  public void setToOrange() {
+    for (int i = 0; i < m_LedBuffer.getLength(); i++) {
+      m_LedBuffer.setRGB(i, 255, 15, 0);
+    }
+  }
+
+  public String getColor() {
+    Color orange = new Color(255, 15, 0);
+    Color purple = new Color(162, 25, 255);
+    Color yellow = new Color(255, 100, 0);
+
+    if (m_LedBuffer.getLED(1).equals(orange)) {
+      return "Orange";
+    } else if (m_LedBuffer.getLED(1).equals(purple)) {
+      return "Purple";
+    } else if (m_LedBuffer.getLED(1).equals(yellow)) {
+      return "Yellow";
+    } else {
+      return "Cannot determine color";
+    }
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    m_addressableLED.setData(m_LEDBuffer);
-    sarahStrobe();
+    m_addressableLED.setData(m_LedBuffer);
   }
 }
