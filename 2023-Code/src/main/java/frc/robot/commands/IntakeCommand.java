1b1d0abcef57b0e0c85986859734afc6f92aaaf2
Leslie <714.leslie.kim@gmail.com>
2023-02-16 21:00:01 -0600

    call armchecker to file name standard which is intakemanager

--- 2023-Code/src/main/java/frc/robot/commands/IntakeCommand.java
+++ 2023-Code/src/main/java/frc/robot/commands/IntakeCommand.java
@@ -11,27 +11,24 @@ import frc.robot.util.IntakeManager;
 public class IntakeCommand extends CommandBase {
 
   IntakeSubsystem m_intakeSubsystem;
-  IntakeManager m_armChecker;
+  IntakeManager m_intakeManager;
 
   /** Creates a new IntakeCommand. */
-  public IntakeCommand(IntakeSubsystem p_intakeSubsystem, IntakeManager p_armChecker) {
+  public IntakeCommand(IntakeSubsystem p_intakeSubsystem, IntakeManager p_intakeManager) {
     m_intakeSubsystem = p_intakeSubsystem;
-    m_armChecker = p_armChecker;
+    m_intakeManager = p_intakeManager;
 
-    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_intakeSubsystem);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (m_armChecker.managerApproved()) {
+    if (m_intakeManager.managerApproved()) {
       m_intakeSubsystem.runIntake();
-      m_armChecker.setIntakeDownWithArmCheck();
+      m_intakeManager.setIntakeDownWithArmCheck();
       m_intakeSubsystem.unsqueeze();
       if (m_intakeSubsystem.isGamePiece()) {
         m_intakeSubsystem.squeeze();
