f9b3107e0135373d1d65e07f70650f89736bab9f
Nathan Poag <886730poa@ames.k12.ia.us>
2023-02-18 15:40:46 -0600

    Update IntakeManager.java

--- 2023-Code/src/main/java/frc/robot/util/IntakeManager.java
+++ 2023-Code/src/main/java/frc/robot/util/IntakeManager.java
@@ -25,7 +25,7 @@ public class IntakeManager {
    * @return If it is OK to run intake-dependant commands.
    */
   public boolean managerApproved() {
-    return m_armSubsystem.getAbsolute Position() <= Constants.ArmConstants.INTAKE_RUNNABLE;
+    return m_armSubsystem.getAbsolutePosition() <= Constants.ArmConstants.INTAKE_RUNNABLE;
   }
 
   /**
