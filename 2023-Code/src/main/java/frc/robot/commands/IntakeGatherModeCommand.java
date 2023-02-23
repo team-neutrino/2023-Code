44af2056587ca15c713e3d822a24df5c6035d9ac
Neutrino Controls <daviddongprog@gmail.com>
2023-02-22 20:28:00 -0600

    Create IntakeGatherModeCommand.java

---
+++ 2023-Code/src/main/java/frc/robot/commands/IntakeGatherModeCommand.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ScoringSubsystem;
+import frc.robot.util.IntakeManager;
+
+/** This command will transfer game piece from intake to arm */
+public class IntakeGatherModeCommand extends CommandBase {
+  IntakeSubsystem m_intakeSubsystem;
+  ArmSubsystem m_armSubsystem;
+  ScoringSubsystem m_scoringSubsystem;
+  IntakeManager m_intakeManager;
+
+  public IntakeGatherModeCommand(
+      IntakeSubsystem p_intakeSubsystem,
+      ArmSubsystem p_armSubsystem,
+      ScoringSubsystem p_scoringSubsystem,
+      IntakeManager p_intakeManager) {
+    m_intakeSubsystem = p_intakeSubsystem;
+    m_armSubsystem = p_armSubsystem;
+    m_scoringSubsystem = p_scoringSubsystem;
+    m_intakeManager = p_intakeManager;
+    addRequirements(m_intakeSubsystem, m_armSubsystem, m_scoringSubsystem);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    m_intakeSubsystem.unsqueeze();
+    m_intakeSubsystem.setIntakeDown();
+    m_intakeSubsystem.runIntake();
+
+    if (m_intakeSubsystem.detectedGamePiece()) {
+      m_intakeSubsystem.stopIntake();
+      m_armSubsystem.setReference(Constants.ArmConstants.FORWARD_DOWN);
+      if (Math.abs(Constants.ArmConstants.FORWARD_DOWN - m_armSubsystem.getPosition()) <= Constants.ArmConstants.ARM_DEADZONE) {
+        m_scoringSubsystem.closeScoring();
+      }
+    }
+   }
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
