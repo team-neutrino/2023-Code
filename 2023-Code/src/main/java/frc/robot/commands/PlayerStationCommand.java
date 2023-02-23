2aa4a39118dce9e0b42638b904ebdcd1b61a1b14
Leslie <714.leslie.kim@gmail.com>
2023-02-22 21:16:45 -0600

    Update PlayerStationCommand.java

--- 2023-Code/src/main/java/frc/robot/commands/PlayerStationCommand.java
+++ 2023-Code/src/main/java/frc/robot/commands/PlayerStationCommand.java
@@ -6,20 +6,25 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ScoringSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 
 public class PlayerStationCommand extends CommandBase {
   private ArmSubsystem m_armSubsystem;
   private ScoringSubsystem m_scoringSubsystem;
+  private IntakeSubsystem m_intakeSubsystem;
+  private Timer timer = new Timer();
 
   /** Creates a new PlayerStationCommand. */
-  public PlayerStationCommand(ArmSubsystem p_armSubsystem, ScoringSubsystem p_scoringSubsystem) {
+  public PlayerStationCommand(ArmSubsystem p_armSubsystem, ScoringSubsystem p_scoringSubsystem, IntakeSubsystem p_intakeSubsystem) {
     m_scoringSubsystem = p_scoringSubsystem;
     m_armSubsystem = p_armSubsystem;
+    m_intakeSubsystem = p_intakeSubsystem;
 
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(p_armSubsystem, m_scoringSubsystem);
+    addRequirements(p_armSubsystem, m_scoringSubsystem, p_intakeSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -30,11 +35,17 @@ public class PlayerStationCommand extends CommandBase {
   @Override
   public void execute() {
     m_armSubsystem.setReference(Constants.ArmConstants.BACK_MID);
+    timer.start();
 
     if (m_scoringSubsystem.detectedGamePiece()) {
       m_scoringSubsystem.closeScoring();
       m_armSubsystem.setReference(Constants.ArmConstants.FORWARD_MID);
     }
+
+    if (timer.get() >= 1) {
+      m_intakeSubsystem.unsqueeze();
+    }
+
   }
 
   // Called once the command ends or is interrupted.
