e10d6b3a31f6984e7001865a9251af6bb5e1837c
Leslie <daviddongprog@gmail.com>
2023-02-25 09:47:14 -0600

    a

--- 2023-Code/src/main/java/frc/robot/TrajectoryConfigConstants.java
+++ 2023-Code/src/main/java/frc/robot/TrajectoryConfigConstants.java
@@ -13,9 +13,9 @@ public class TrajectoryConfigConstants {
   public static final double K_TRACK_WIDTH_METERS = 0.635;
   public static final DifferentialDriveKinematics K_DRIVE_KINEMATICS =
       new DifferentialDriveKinematics(K_TRACK_WIDTH_METERS);
-  public static final double K_MAX_SPEED_METERS_PER_SECOND = 3;
-  public static final double K_HALF_SPEED_METERS_PER_SECOND = 1.5;
-  public static final double K_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 2.5;
+  public static final double K_MAX_SPEED_METERS_PER_SECOND = 5;
+  public static final double K_HALF_SPEED_METERS_PER_SECOND = 2.5;
+  public static final double K_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 2;
   public static final double K_RAMSETE_BETA = 2;
   public static final double K_RAMSETE_ZETA = 0.7;
   public static final double KP_DRIVE_VEL = .44295;
