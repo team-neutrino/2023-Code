f5086dcce03fc1f29467716975aee2dbec69062f
Leslie <daviddongprog@gmail.com>
2023-02-25 09:45:41 -0600

    track width meters
    25 inches

--- 2023-Code/src/main/java/frc/robot/TrajectoryConfigConstants.java
+++ 2023-Code/src/main/java/frc/robot/TrajectoryConfigConstants.java
@@ -10,11 +10,11 @@ public class TrajectoryConfigConstants {
   // COPIED FROM ATLAS
   public static final double KV_VOLT_SECONDS_PER_METER = 2.363;
   public static final double KA_VOLT_SECONDS_SQUARED_PER_METER = 0.3851;
-  public static final double K_TRACK_WIDTH_METERS = 0.668;
+  public static final double K_TRACK_WIDTH_METERS = 0.635;
   public static final DifferentialDriveKinematics K_DRIVE_KINEMATICS =
       new DifferentialDriveKinematics(K_TRACK_WIDTH_METERS);
   public static final double K_MAX_SPEED_METERS_PER_SECOND = 3;
-  public static final double K_HALF_SPEED_METERS_PER_SECOND = 0.5;
+  public static final double K_HALF_SPEED_METERS_PER_SECOND = 1.5;
   public static final double K_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 2.5;
   public static final double K_RAMSETE_BETA = 2;
   public static final double K_RAMSETE_ZETA = 0.7;
@@ -29,7 +29,7 @@ public class TrajectoryConfigConstants {
           10);
 
   public static final TrajectoryConfig m_ForwardConfig =
-      new TrajectoryConfig(10, K_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED)
+      new TrajectoryConfig(K_MAX_SPEED_METERS_PER_SECOND, K_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED)
           .setKinematics(K_DRIVE_KINEMATICS)
           .addConstraint(m_autoVoltageConstraint);
 }
