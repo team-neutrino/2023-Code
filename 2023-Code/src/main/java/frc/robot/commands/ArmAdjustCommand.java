9e60a8ef75e22e52800aa3744432136c4030ed6d
Leslie <daviddongprog@gmail.com>
2023-03-11 13:31:08 -0600

    RENAME

--- 2023-Code/src/main/java/frc/robot/commands/ArmAdjustCommand.java
+++ 2023-Code/src/main/java/frc/robot/commands/ArmAdjustCommand.java
@@ -37,12 +37,12 @@ public class ArmAdjustCommand extends CommandBase {
 
     if (m_driverController.getRightY() < -Constants.ArmConstants.ARM_INPUT_DEADZONE) {
       voltage =
-          m_armSubsystem.limitAmount(
+          m_armSubsystem.limitArmVoltage(
               m_driverController.getRightY() / Constants.ArmConstants.SCALE_FACTOR);
       targetAngle = m_armSubsystem.getAbsolutePosition();
     } else if (m_driverController.getRightY() > Constants.ArmConstants.ARM_INPUT_DEADZONE) {
       voltage =
-          m_armSubsystem.limitAmount(
+          m_armSubsystem.limitArmVoltage(
               m_driverController.getRightY() / Constants.ArmConstants.SCALE_FACTOR);
       targetAngle = m_armSubsystem.getAbsolutePosition();
     } else {
