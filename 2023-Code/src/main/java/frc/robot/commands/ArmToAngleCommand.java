6c2775df85b6cd21ce66ae07307a7a2d29cd12c4
Leslie <daviddongprog@gmail.com>
2023-03-17 17:32:12 -0500

    spotless

--- 2023-Code/src/main/java/frc/robot/commands/ArmToAngleCommand.java
+++ 2023-Code/src/main/java/frc/robot/commands/ArmToAngleCommand.java
@@ -78,18 +78,17 @@ public class ArmToAngleCommand extends CommandBase {
   @Override
   public void execute() {
     if (m_buttoncheck) {
-      
+
       if (m_ledSubsystem.getColor() == LEDColor.YELLOW) {
         voltage =
             m_pidController.run(
                 m_armSubsystem.getAbsoluteArmPosition(), Constants.ArmConstants.BACK_MID);
         m_armSubsystem.smartArmSet(voltage);
-      }
-      else {
+      } else {
         voltage =
-        m_pidController.run(
-            m_armSubsystem.getAbsoluteArmPosition(), Constants.ArmConstants.QUASI_BACK_MID);
-    m_armSubsystem.smartArmSet(voltage);
+            m_pidController.run(
+                m_armSubsystem.getAbsoluteArmPosition(), Constants.ArmConstants.QUASI_BACK_MID);
+        m_armSubsystem.smartArmSet(voltage);
       }
     } else {
       voltage = m_pidController.run(m_armSubsystem.getAbsoluteArmPosition(), m_targetAngle);
