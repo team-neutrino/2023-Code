ab740f1880d276d088aa354f0748efa6aeff5be0
gurellia53 <daviddongprog@gmail.com>
2023-03-19 14:08:42 -0500

    delete print

--- 2023-Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ 2023-Code/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -71,7 +71,6 @@ public class ArmSubsystem extends SubsystemBase {
   public void limitHeight() {
     if ((getAbsoluteArmPosition() <= Constants.ArmConstants.FORWARD_ARM_HEIGHT_LIMIT && getAbsoluteArmPosition() >= Constants.ArmConstants.BACKWARD_ARM_HEIGHT_LIMIT)
       && getEncoderDistance() <= 0) {
-      System.out.println("sarah's thing");
       setTelescope(ArmConstants.TELESCOPE_HEIGHT_LIMIT_RETRACT_SPEED);
     }
   }
