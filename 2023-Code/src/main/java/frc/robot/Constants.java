563168c24d178a405a74c0fff69d7031b3496168
meabhhenn <124942237+meabhhenn@users.noreply.github.com>
2023-04-12 17:15:07 -0500

    change speed of telescoping

--- 2023-Code/src/main/java/frc/robot/Constants.java
+++ 2023-Code/src/main/java/frc/robot/Constants.java
@@ -79,7 +79,7 @@ public final class Constants {
   public static class TelescopeConstants {
 
     public static final double TELESCOPE_EXTEND_SPEED = 0.5; // note: setInverted is true
-    public static final double TELESCOPE_RETRACT_SPEED = -0.5;
+    public static final double TELESCOPE_RETRACT_SPEED = -0.85;
     public static final double TELESCOPE_EXTEND_LIMIT = -5500;
     public static final double TELESCOPING_DEADZONE = 250;
 
