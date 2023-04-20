2ff92d8af209f817a205fae00ee9a893a8319a9f
meabhhenn <124942237+meabhhenn@users.noreply.github.com>
2023-04-20 18:45:35 -0500

    this command doesn't do anything yet

---
+++ 2023-Code/src/main/java/frc/robot/commands/TurnAroundCommand.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.SubsystemContainer;
+import frc.robot.subsystems.DriveTrainSubsystem;
+
+public class TurnAroundCommand extends CommandBase {
+  DriveTrainSubsystem m_drivetrainSubsystem; 
+
+  public TurnAroundCommand(SubsystemContainer p_subsystemContainer) {
+    m_drivetrainSubsystem = p_subsystemContainer.getDriveTrainSubsystem();
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {}
+
+  @Override
+  public void end(boolean interrupted) {}
+
+  @Override
+  public boolean isFinished() {
+    
+    return false;
+  }
+}
