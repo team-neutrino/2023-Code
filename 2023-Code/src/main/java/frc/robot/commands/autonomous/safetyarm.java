d74ad6440fd8bdb81073a37cfc046b87b2af2631
Neutrino Controls <117839284+ssarahyao@users.noreply.github.com>
2023-06-30 19:29:17 -0500

    Create safetyarm.java

---
+++ 2023-Code/src/main/java/frc/robot/commands/autonomous/safetyarm.java
@@ -0,0 +1,18 @@
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.SubsystemContainer;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.TelescopeSubsystem;
+import frc.robot.util.Limiter;
+import frc.robot.util.ViennaPIDController;
+
+public class safetyarm extends CommandBase{
+    
+}
